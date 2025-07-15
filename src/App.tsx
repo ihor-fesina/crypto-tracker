ce3333acdd3a017c64164284e60487a969ecd426
ihor-fesina <fesinaihor@gmail.com>
2025-07-15 09:36:56 +0200

    configured store and api service

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,21 @@ import {Route, Routes} from "react-router-dom";
 import Layout from "./layout/Layout";
 import Home from "./pages/Home";
 import Coin from "./pages/Coin";
-
+import store from './app/store'
+import {Provider} from "react-redux";
 function App() {
 
 
-  return (
-      <Routes>
-          <Route element={<Layout/>}>
-          <Route path='/' element={<Home/>}/>
-          <Route path='/coin/:coinId' element={<Coin/>}/>
-          </Route>
-      </Routes>
-  )
+    return (
+        <Provider store={store}>
+        <Routes>
+            <Route element={<Layout/>}>
+                <Route path='/' element={<Home/>}/>
+                <Route path='/coin/:coinId' element={<Coin/>}/>
+            </Route>
+        </Routes>
+        </Provider>
+    )
 }
 
 export default App
---
+++ src/services/cryptoApi.ts
@@ -0,0 +1,24 @@
+import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react"
+
+const cryptoApiHeaders = {
+    'x-rapidapi-host': import.meta.env.VITE_CRYPTO_RAPIDAPI_HOST,
+    'x-rapidapi-key': import.meta.env.VITE_RAPIDAPI_KEY,
+};
+const baseUrl = import.meta.env.VITE_CRYPTO_API_URL
+
+const createRequest = (url: string) => ({ url, headers: cryptoApiHeaders });
+
+export const cryptoApi = createApi({
+    reducerPath: "cryptoApi",
+    baseQuery: fetchBaseQuery({baseUrl}),
+    endpoints: (builder) => ({
+        getCryptos: builder.query({
+            query: () => createRequest(`/coins`),
+            // query: (count: number) => createRequest(`/coins?limit=${count}`),
+        })
+    })
+})
+
+export const {
+    useGetCryptosQuery
+} = cryptoApi;
