579b3c8fd35bafd1924bfc29b2c05f6752a1c8a4
ihor-fesina <fesinaihor@gmail.com>
2025-07-16 17:10:05 +0200

    header styles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,13 @@
+import { Link } from 'react-router-dom';
+
 const Header = () => {
     return (
-        <header>
-            <h2>Header</h2>
+        <header className="text-white py-4 text-center md:px-8">
+            <div className="border-b-2 border-white p-4 flex justify-start ">
+            <Link to="/" className="text-xl font-semibold inline-block uppercase">
+                Crypto
+            </Link>
+            </div>
         </header>
     );
 };
